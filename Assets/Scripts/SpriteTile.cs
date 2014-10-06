5aaf34a842c6adcbe8c5a57428bb7c1d66b7e4f7
mintleaf <mintchipleaf@gmail.com>
2014-10-06 00:10:57 -0400

    Add beginning of tiling script

--- Assets/Scripts/SpriteTile.cs
+++ Assets/Scripts/SpriteTile.cs
@@ -1,22 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class SpriteTile : MonoBehaviour {
 
-	private Vector3 ThisY;
-	private Vector3 CameraY;
+	private float ThisYBound;
+	private float ThisYMin;
+	private float CameraYMax;
+	private float CameraYMin;
 
 	// Use this for initialization
 	void Start () {
-	
+		//Debug.Log(renderer.bounds.min.y);
+		//Debug.Log(renderer.bounds.max.y);
+		Debug.Log("extents y  "+this.renderer.bounds.extents.y);
+		Debug.Log("camera y min  "+Camera.main.ViewportToWorldPoint(new Vector3(0,0)).y);
+		Debug.Log("camera y max  "+Camera.main.ViewportToWorldPoint(new Vector3(1,1)).y);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		ThisY = this.transform.position.y;
-		CameraY = Camera.main.transform.position;
-		if (ThisY >= CameraY){
 
+		ThisYMin = this.renderer.bounds.min.y;
+		CameraYMax = Camera.main.ViewportToWorldPoint(new Vector3(1,1)).y;
+
+		if (ThisYMin >= CameraYMax){
+			ThisYBound = this.renderer.bounds.extents.y;
+			CameraYMin = Camera.main.ViewportToWorldPoint(new Vector3(0,0)).y;
+
+			this.transform.position = new Vector3(this.transform.position.x, CameraYMin - ThisYBound);
 			//Try ThisY + Bounds.extents(y) >= ViewPortToWorldPoint(camera.main.transform.positon.y + camera's Bounds.extents(t))
 		}
 	}
